60d3f23be055409f70e80af700db8fa2d96e16f5
Piêtro Braga <pietrobragaprofissional@gmail.com>
2022-08-13 22:04:52 -0300

    Paginacao com findAll e criacao do ClientDTO

--- src/main/java/com/pietrobraga/projetoclientcrud/resources/ClientResource.java
+++ src/main/java/com/pietrobraga/projetoclientcrud/resources/ClientResource.java
@@ -1,14 +1,16 @@
 package com.pietrobraga.projetoclientcrud.resources;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.pietrobraga.projetoclientcrud.entities.Client;
+import com.pietrobraga.projetoclientcrud.dto.ClientDTO;
 import com.pietrobraga.projetoclientcrud.services.ClientService;
 
 @RestController
@@ -19,8 +21,15 @@ public class ClientResource {
 	ClientService clientService;
 	
 	@GetMapping
-	public ResponseEntity<List<Client>> findAll(){
-		return ResponseEntity.ok().body(clientService.findAll());
+	public ResponseEntity<Page<ClientDTO>> findAll(
+			@RequestParam(value = "page", defaultValue = "0") Integer page,
+			@RequestParam(value = "linesPerPage", defaultValue = "12") Integer linesPerPage,
+			@RequestParam(value = "direction", defaultValue = "DESC") String direction,
+			@RequestParam(value = "orderBy", defaultValue = "name") String orderBy)
+	{
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		Page<ClientDTO> list = clientService.findAllPaged(pageRequest);
+		return ResponseEntity.ok().body(list);
 	}
 	
 }
--- src/main/java/com/pietrobraga/projetoclientcrud/services/ClientService.java
+++ src/main/java/com/pietrobraga/projetoclientcrud/services/ClientService.java
@@ -1,11 +1,12 @@
 package com.pietrobraga.projetoclientcrud.services;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.pietrobraga.projetoclientcrud.dto.ClientDTO;
 import com.pietrobraga.projetoclientcrud.entities.Client;
 import com.pietrobraga.projetoclientcrud.repositories.ClientRepository;
 
@@ -16,8 +17,9 @@ public class ClientService {
 	ClientRepository repo;
 	
 	@Transactional(readOnly = true)
-	public List<Client> findAll(){
-		return repo.findAll();
+	public Page<ClientDTO> findAllPaged(PageRequest pageRequest){
+		Page<Client> clients = repo.findAll(pageRequest);
+		return clients.map(x -> new ClientDTO(x));
 	}
 	
 }
