8f863ee4fc39b80be32d409303a136710673a17e
Piêtro Braga <pietrobragaprofissional@gmail.com>
2022-08-13 21:13:30 -0300

    Criando projeto

---
+++ src/main/java/com/pietrobraga/projetoclientcrud/ProjetoclientcrudApplication.java
@@ -0,0 +1,13 @@
+package com.pietrobraga.projetoclientcrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoclientcrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoclientcrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pietrobraga/projetoclientcrud/ProjetoclientcrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pietrobraga.projetoclientcrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoclientcrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
